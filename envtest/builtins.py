38ae56eddf310e4b68820850454feb01b1bd3d37
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2025-10-06 11:52:24 +0100

    More committing

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'sin']
 
 
 def rand_array(shape):
@@ -13,3 +13,6 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def sin(x):
+    return np.sin(x)
---
+++ scripts/my_sin.py
@@ -0,0 +1,5 @@
+from envtest import sin
+import numpy as np
+
+x = np.linspace(-2*np.pi, 2*np.pi, 10)
+print(sin(x))
