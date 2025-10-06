4ecb41ac6d9b33a7a2cc5fecdbdd8d94dacc7025
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2025-10-06 11:48:48 +0100

    Commit for exercise

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,4 +1,20 @@
-from envtest import rand_array
+from envtest import smooth_image
+
+from skimage import data
+import matplotlib.pyplot as plt
+
+image = data.camera()
+sigma = 5
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1, 2, 1)
+plt.imshow(image)
+
+f.add_subplot(1, 2, 2)
+plt.imshow(smoothed_image)
+plt.show(block=True)
 
 shape = (3, 3)
 
